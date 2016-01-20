a3f8f2001a55468a173a01b0aa093986b6abf5df
Zhili He <zhili850702@gmail.com>
2016-01-20 12:41:39 +0800

    Generating containers with connect() from react reduxt(FooterLink)

--- es6/todo.js
+++ es6/todo.js
@@ -77,43 +77,36 @@ const Link = ({
 	);
 };
 
-class FilterLink extends React.Component {
-	componentDidMount() {
-		const { store } = this.context;
-		store.subscribe(() =>
-			this.forceUpdate()
-		);
-	}
-
-	componentWillUnmount() {
-		this.unsubscribe();
-	}
-
-	render() {
-		const props = this.props;
-		const { store } = this.context;
-		const state = store.getState();
-
-		return (
-			<Link
-				active={
-					props.filter === state.visibilityFilter
-				}
-				onClick={() => {
-					store.dispatch({
-						type: 'SET_VISIBILITY_FILTER',
-						filter: props.filter
-					})
-				}}
-			>
-				{props.children}
-			</Link>
-		);
-	}
-}
-FilterLink.contextTypes = {
-	store: React.PropTypes.object
+const mapStateToLinkState = (
+	state,
+	ownProps
+) => {
+	return {
+		active: 
+			ownProps.filter ===
+			state.visibilityFilter
+	};
 };
+
+const mapDispathToLinkProps = (
+	dispatch,
+	ownProps
+) => {
+	return {
+		onClick: () => {
+			dispatch({
+				type: 'SET_VISIBILITY_FILTER',
+				filter: ownProps.filter
+			});
+		}
+	};
+}
+
+const FilterLink = connect(
+	mapStateToTodoListProps,
+	mapDispathToLinkProps
+)(Link);
+
 
 const Footer = () => (
 
@@ -181,7 +174,7 @@ let AddTodo = ({ dispatch }) => {
 				input = node;
 			}} />
 			<button onClick={() => {
-				store.dispatch({
+				dispatch({
 					type: 'ADD_TODO',
 					id: nextTodoId++,
 					text: input.value
